c7eab18deda4ed415e2fdfb23b20dd7fad567616
sz6084 <sz6084@users.noreply.github.com>
2025-03-24 19:49:50 -0700

    Added a temporary firmware program

---
+++ Firmware/main.py
@@ -0,0 +1,50 @@
+# You import all the IOs of your board
+import board
+
+# These are imports from the kmk library
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+
+# This is the main instance of your keyboard
+keyboard = KMKKeyboard()
+
+# Add the macro extension
+macros = Macros()
+keyboard.modules.append(macros)
+
+# Define your pins here!
+# PINS = [board.D3, board.D4, board.D2, board.D1]
+keyboard.row_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5)
+keyboard.col_pins = (board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15, board.GP16, board.GP17, board.GP18, board.GP19, board.GP20, board.GP21, board.GP22, board.GP26)
+
+# Tell kmk we are not using a key matrix
+# keyboard.matrix = KeysScanner(
+#     pins=PINS,
+#     value_when_pressed=False,
+# )
+
+# Here you define the buttons corresponding to the pins
+# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
+# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
+keyboard.keymap = [
+    [  # Base layer
+        KC.ESC,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.MINS,  KC.EQL,   KC.BSPC,  KC.INS,   KC.HOME,  KC.PGUP,
+        KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.LBRC,  KC.RBRC,  KC.BSLS,  KC.DEL,   KC.END,   KC.PGDN,
+        KC.CAPS, KC.A,    KC.S,    KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN,  KC.QUOT,  KC.ENT,
+        KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,  KC.UP,
+        KC.LCTL, KC.LGUI, KC.LALT, KC.SPC,  KC.RALT, KC.RGUI, KC.RCTL,  KC.LEFT,  KC.DOWN,  KC.RGHT
+    ],
+    [  # Layer 1 (Function layer)
+        KC.GRV,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,   KC.DEL,   KC.PSCR,  KC.SLCK,  KC.PAUS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,  KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS
+    ]
+]
+
+# Start kmk!
+if __name__ == '__main__':
+    keyboard.go()
